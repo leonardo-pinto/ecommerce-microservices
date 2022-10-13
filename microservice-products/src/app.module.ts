5f73f211fa3c1698167afca33341a952a0c602f6
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-10-13 20:42:44 -0300

    feat(microservices-product): add mongoose to app module

--- microservice-products/src/app.module.ts
+++ microservice-products/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.v0chnrz.mongodb.net/products?retryWrites=true&w=majority`,
+    ),
+    ProductsModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
