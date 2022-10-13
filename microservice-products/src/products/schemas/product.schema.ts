0a442d42066cb023969dbd4715fdd5c3386954d7
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-10-13 20:25:42 -0300

    feat(microservice-products): create product schema

---
+++ microservice-products/src/products/schemas/product.schema.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+
+@Schema({ timestamps: true, collection: 'products' })
+export class Product extends mongoose.Document {
+  @Prop()
+  name: string;
+
+  @Prop()
+  price: number;
+
+  @Prop()
+  quantity: number;
+
+  @Prop()
+  description: string;
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
