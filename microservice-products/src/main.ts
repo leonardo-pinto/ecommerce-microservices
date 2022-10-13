437e6eec0c2d1609827b5c79480d33c542365ed9
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-10-13 20:41:38 -0300

    feat(microservices-product): add prefix, pipes, filters and interceptors to main file

--- microservice-products/src/main.ts
+++ microservice-products/src/main.ts
@@ -1,8 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './filters/http-exception.filter';
+import { TimeoutInterceptor } from './interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1/');
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new AllExceptionsFilter());
+  app.useGlobalInterceptors(new TimeoutInterceptor());
   await app.listen(3000);
 }
 bootstrap();
