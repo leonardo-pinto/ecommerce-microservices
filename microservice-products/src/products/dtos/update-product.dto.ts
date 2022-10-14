194989d0081252d5ab5c0e3a9454c6b8d48a8a19
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-10-14 19:02:58 -0300

    refactor(microservice-products): update UpdateProductDto as received from rabbit mq

--- microservice-products/src/products/dtos/update-product.dto.ts
+++ microservice-products/src/products/dtos/update-product.dto.ts
@@ -1,11 +1,30 @@
-import { IsOptional, IsNumber } from 'class-validator';
+import { Type } from 'class-transformer';
+import {
+  IsOptional,
+  IsNumber,
+  IsString,
+  IsNotEmpty,
+  IsNotEmptyObject,
+  ValidateNested,
+} from 'class-validator';
 
-export class UpdateProductDto {
+export class UpdateProduct {
   @IsNumber()
   @IsOptional()
-  price: number;
+  price?: number;
 
   @IsNumber()
   @IsOptional()
-  quantity: number;
+  quantity?: number;
+}
+
+export class UpdateProductDataDto {
+  @IsNotEmptyObject()
+  @ValidateNested()
+  @Type(() => UpdateProduct)
+  updateProductDto: UpdateProduct;
+
+  @IsString()
+  @IsNotEmpty()
+  id: string;
 }
