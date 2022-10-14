01d4d6548fbef6683de79af936cf724ac08e3fd0
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-10-14 18:55:20 -0300

    feat(api-gateway): update main.ts

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,8 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new TimeoutInterceptor());
   await app.listen(3000);
 }
 bootstrap();
