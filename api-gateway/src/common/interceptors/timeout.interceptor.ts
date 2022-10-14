9040f1c9a1a0abbc5ec7f4d2250549de8b063c38
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-10-14 18:54:15 -0300

    feat(api-gateway): add timeout interceptor

---
+++ api-gateway/src/common/interceptors/timeout.interceptor.ts
@@ -0,0 +1,11 @@
+import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { Observable, timeout } from 'rxjs';
+
+export class TimeoutInterceptor implements NestInterceptor {
+    intercept(
+        context: ExecutionContext,
+        next: CallHandler<any>
+    ): Observable<any> | Promise<Observable<any>> {
+        return next.handle().pipe(timeout(10000));
+    }
+}
