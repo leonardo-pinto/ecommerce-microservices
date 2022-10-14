ab0ae22eb1b7f188e270c5f6745aed3414a185f9
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-10-14 19:03:44 -0300

    refactor(microservice-products): update controller to use microservices

--- microservice-products/src/products/products.controller.ts
+++ microservice-products/src/products/products.controller.ts
@@ -1,31 +1,67 @@
-import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
+import {
+  Ctx,
+  EventPattern,
+  MessagePattern,
+  Payload,
+  RmqContext,
+} from '@nestjs/microservices';
 import { CreateProductDto } from './dtos/create-product.dto';
-import { UpdateProductDto } from './dtos/update-product.dto';
 import { ProductsService } from './products.service';
 import { Product } from './schemas/product.schema';
+import { AckErrors } from '../utils/ack-errors';
+import { UpdateProductDataDto } from './dtos/update-product.dto';
 
-@Controller('products')
+@Controller()
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
-  @Post()
+  @EventPattern('create-product')
   async createProduct(
-    @Body() createProductDto: CreateProductDto,
-  ): Promise<void> {
-    await this.productsService.createProduct(createProductDto);
+    @Payload() data: CreateProductDto,
+    @Ctx() context: RmqContext,
+  ) {
+    const channel = context.getChannelRef();
+    const originalMessage = context.getMessage();
+
+    try {
+      await this.productsService.createProduct(data);
+      await channel.ack(originalMessage);
+    } catch (error) {
+      if (AckErrors.hasAckErrors(error.message)) {
+        await channel.ack(originalMessage);
+      }
+    }
   }
 
-  @Get()
-  async getAllProducts(): Promise<Product[]> {
-    const result = await this.productsService.getAllProducts();
-    return result;
+  @MessagePattern('get-products')
+  async getAllProducts(@Ctx() context: RmqContext): Promise<Product[]> {
+    const channel = context.getChannelRef();
+    const originalMessage = context.getMessage();
+
+    try {
+      const result = await this.productsService.getAllProducts();
+      return result;
+    } finally {
+      await channel.ack(originalMessage);
+    }
   }
 
-  @Put(':id')
+  @EventPattern('update-product')
   async updateProduct(
-    @Body() updateProductDto: UpdateProductDto,
-    @Param('id') id: string,
-  ): Promise<void> {
-    await this.productsService.updateProduct(updateProductDto, id);
+    @Payload() data: UpdateProductDataDto,
+    @Ctx() context: RmqContext,
+  ) {
+    const channel = context.getChannelRef();
+    const originalMessage = context.getMessage();
+    const { updateProductDto, id } = data;
+    try {
+      await channel.ack(originalMessage);
+      await this.productsService.updateProduct(updateProductDto, id);
+    } catch (error) {
+      if (AckErrors.hasAckErrors(error.message)) {
+        await channel.ack(originalMessage);
+      }
+    }
   }
 }
