5910eb71a4533f0224875fee9d685c00d87877b7
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-10-14 18:55:09 -0300

    feat(api-gateway): update app module

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ProxyRMQModule } from './proxyrmq/proxyrmq.module';
+import { ConfigModule } from '@nestjs/config';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ProxyRMQModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    ProductsModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
