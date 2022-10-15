c0d94eacd07b778dcdb7fa4d7590b3bb083fd60b
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-10-15 10:14:44 -0300

    feat(api-gateway): add http exception filter to main.ts

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
 
 async function bootstrap() {
@@ -8,6 +9,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TimeoutInterceptor());
+  app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(3000);
 }
 bootstrap();
