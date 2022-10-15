4f1cee402133fbe67d721e85db781da01fcc603d
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-10-15 10:13:40 -0300

    feat(api-gateway): add jwt strategy using passport

---
+++ api-gateway/src/auth/jwt.strategy.ts
@@ -0,0 +1,30 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { passportJwtSecret } from 'jwks-rsa';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      audience: process.env.AWS_COGNITO_CLIENT_ID,
+      issuer: `https://cognito-idp.${process.env.AWS_COGNITO_REGION}.amazonaws.com/${process.env.AWS_COGNITO_USER_POOL_ID}`,
+      algorithms: ['RS256'],
+      secretOrKeyProvider: passportJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `https://cognito-idp.${process.env.AWS_COGNITO_REGION}.amazonaws.com/${process.env.AWS_COGNITO_USER_POOL_ID}/.well-known/jwks.json`,
+      }),
+    });
+  }
+
+  public async validate(payload: any) {
+    return {
+      userId: payload.sub,
+      email: payload.email,
+    };
+  }
+}
