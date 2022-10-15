252e6d226c9ee7c234f2b0554f555cfd9916232a
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-10-15 10:15:25 -0300

    feat(api-gateway): add aws and auth modules to app.module

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -2,14 +2,19 @@ import { Module } from '@nestjs/common';
 import { ProxyRMQModule } from './proxyrmq/proxyrmq.module';
 import { ConfigModule } from '@nestjs/config';
 import { ProductsModule } from './products/products.module';
+import { AwsModule } from './aws/aws.module';
+import { AuthModule } from './auth/auth.module';
+import { AwsCognitoService } from './aws/aws-cognito.service';
 
 @Module({
   imports: [
     ProxyRMQModule,
     ConfigModule.forRoot({ isGlobal: true }),
     ProductsModule,
+    AwsModule,
+    AuthModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [AwsCognitoService],
 })
 export class AppModule {}
