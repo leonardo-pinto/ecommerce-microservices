8b07052eb8b0f16aaea15d18a31475980a52bebb
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-10-18 15:01:49 -0300

    feat(microservices-products): add new patterns to controller

--- microservice-products/src/products/products.controller.ts
+++ microservice-products/src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
 import {
   Ctx,
   EventPattern,
@@ -8,12 +8,14 @@ import {
 } from '@nestjs/microservices';
 import { CreateProductDto } from './dtos/create-product.dto';
 import { ProductsService } from './products.service';
-import { Product } from './schemas/product.schema';
 import { AckErrors } from '../utils/ack-errors';
-import { UpdateProductDataDto } from './dtos/update-product.dto';
+import { UpdateProductDto } from './dtos/update-product.dto';
+import { Product } from './interfaces/product.interface';
 
 @Controller()
 export class ProductsController {
+  private logger = new Logger(ProductsController.name);
+
   constructor(private readonly productsService: ProductsService) {}
 
   @EventPattern('create-product')
@@ -21,6 +23,7 @@ export class ProductsController {
     @Payload() data: CreateProductDto,
     @Ctx() context: RmqContext,
   ) {
+    this.logger.log(`Data: ${JSON.stringify(data)}`);
     const channel = context.getChannelRef();
     const originalMessage = context.getMessage();
 
@@ -47,18 +50,40 @@ export class ProductsController {
     }
   }
 
-  @EventPattern('update-product')
-  async updateProduct(
-    @Payload() data: UpdateProductDataDto,
+  @MessagePattern('get-product-by-id')
+  async getProductById(
+    @Payload() id: string,
     @Ctx() context: RmqContext,
-  ) {
+  ): Promise<Product> {
     const channel = context.getChannelRef();
     const originalMessage = context.getMessage();
-    const { updateProductDto, id } = data;
+
+    try {
+      const result = await this.productsService.getProductById(id);
+      await channel.ack(originalMessage);
+      return result;
+    } catch (error) {
+      this.logger.error(`Error: ${JSON.stringify(error)}`);
+      if (AckErrors.hasAckErrors(error.message)) {
+        await channel.ack(originalMessage);
+      }
+    }
+  }
+
+  @EventPattern('update-product-quantity')
+  async updateProduct(
+    @Payload() data: UpdateProductDto,
+    @Ctx() context: RmqContext,
+  ) {
+    this.logger.log(`Data: ${JSON.stringify(data)}`);
+    const channel = context.getChannelRef();
+    const originalMessage = context.getMessage();
+    const { quantity, id } = data;
     try {
       await channel.ack(originalMessage);
-      await this.productsService.updateProduct(updateProductDto, id);
+      await this.productsService.updateProduct(quantity, id);
     } catch (error) {
+      this.logger.error(`Error: ${JSON.stringify(error)}`);
       if (AckErrors.hasAckErrors(error.message)) {
         await channel.ack(originalMessage);
       }
