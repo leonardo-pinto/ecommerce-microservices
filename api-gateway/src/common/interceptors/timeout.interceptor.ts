8f7afc0543e26f62c4c019f66cd9d902e5b45591
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-10-19 09:08:28 -0300

    chore(api-gateway): fix TimeoutInterceptor indentation

--- api-gateway/src/common/interceptors/timeout.interceptor.ts
+++ api-gateway/src/common/interceptors/timeout.interceptor.ts
@@ -2,10 +2,10 @@ import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
 import { Observable, timeout } from 'rxjs';
 
 export class TimeoutInterceptor implements NestInterceptor {
-    intercept(
-        context: ExecutionContext,
-        next: CallHandler<any>
-    ): Observable<any> | Promise<Observable<any>> {
-        return next.handle().pipe(timeout(10000));
-    }
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    return next.handle().pipe(timeout(10000));
+  }
 }
